9beb8de9c91a6feaa7a2f2737231493301e011b3
wrona777 <106965107+wrona777@users.noreply.github.com>
2024-04-05 12:48:31 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import List from './components/List'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <List />
+    </div>
+  );
+}
+
+export default App;
